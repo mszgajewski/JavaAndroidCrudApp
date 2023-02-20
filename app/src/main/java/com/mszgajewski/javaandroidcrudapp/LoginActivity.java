191dd413bcbf6da1577ef654bdf63c97f173e1de
mszgajewski <mszgajewski@gmail.com>
2023-02-20 21:25:21 +0100

    Commit

--- app/src/main/java/com/mszgajewski/javaandroidcrudapp/LoginActivity.java
+++ app/src/main/java/com/mszgajewski/javaandroidcrudapp/LoginActivity.java
@@ -7,17 +7,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
+
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
 import com.mszgajewski.javaandroidcrudapp.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
